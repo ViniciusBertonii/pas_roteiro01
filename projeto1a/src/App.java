88210611a4ba20c049870eaa53f369cd4d693b5d
Marcos Vinicius Bertoni <161330161+ViniciusBertonii@users.noreply.github.com>
2025-03-07 01:12:57 +0000

    Commit 1

---
+++ projeto1a/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    System.out.println("\nHello!");
+    String name = System.getProperty("user.name"); System.out.println("User Name: "+name+"\n");
+    } }
