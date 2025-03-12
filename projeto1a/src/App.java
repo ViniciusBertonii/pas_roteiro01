9fa5fbcc78495b11c584333fc50f5f1a918d82fa
Marcos Vinicius Bertoni <161330161+ViniciusBertonii@users.noreply.github.com>
2025-03-12 00:26:58 +0000

    nome alterado

--- projeto1a/src/App.java
+++ projeto1a/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
     System.out.println("\nHello!");
-    String name = System.getProperty("user.name"); System.out.println("Nome de usuario: "+name+"\n");
+    String name = System.getProperty("user.name"); System.out.println("Nome de usuario do sistema: "+name+"\n");
     } }
