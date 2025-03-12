c0e29d40d6a1da58391fb999eeb50f93884f16e4
Marcos Vinicius Bertoni <161330161+ViniciusBertonii@users.noreply.github.com>
2025-03-11 21:25:23 -0300

    Commit 1

--- projeto1a/src/App.java
+++ projeto1a/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
     System.out.println("\nHello!");
-    String name = System.getProperty("user.name"); System.out.println("User Name: "+name+"\n");
+    String name = System.getProperty("user.name"); System.out.println("Nome de usuario: "+name+"\n");
     } }
